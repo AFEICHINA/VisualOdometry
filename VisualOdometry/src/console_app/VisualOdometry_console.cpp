28d0438f05ed0cc9742cc70a831c897a2bd93383
afei <80672451@qq.com>
2020-05-19 13:55:38 +0800

    Update VisualOdometry_console.cpp

--- VisualOdometry/src/console_app/VisualOdometry_console.cpp
+++ VisualOdometry/src/console_app/VisualOdometry_console.cpp
@@ -1,5 +1,9 @@
 #include "main_engine/MainEngine.h"
 
 int main(int argc,char** argv) {
+        MainEngine MainEngine;
+        MainEngine.ReadConfigrationFiles(argc,argv);
+        MainEngine.setParameters();
+        MainEngine.run();
 	return 0;
 }
